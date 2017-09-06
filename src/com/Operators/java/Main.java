32d0e04e9333e3ad2bbbe864cb371de0e6e26a18
johnpauwels74 <johnpauwels74@gmail.com>
2017-09-06 10:36:07 -0500

    Initial commit

---
+++ src/com/Operators/java/Main.java
@@ -0,0 +1,27 @@
+package com.Operators.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int additionvalue = 2+6;
+            System.out.println("2 + 6 = " + additionvalue);
+
+        int subtractionvalue = 567-264;
+            System.out.println("567 - 264 = " + subtractionvalue);
+
+        double multvalue = 54*753;
+            System.out.println("54 * 753 = " + multvalue);
+
+        double divvalue = 234/6;
+            System.out.println("234 / 6 = " + divvalue);
+
+        boolean booleanvalue;
+        if (2 > 6){
+            booleanvalue = true;
+        }
+        else {
+            booleanvalue = false;
+        }
+            System.out.println("2 > 6 = " + booleanvalue);
+    }
+}
